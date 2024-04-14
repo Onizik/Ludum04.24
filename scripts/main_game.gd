fef5f761927c7a9b5031ca6b64652eb7583ab1bb
Onizik <Onizik@mail.ru>
2024-04-14 13:50:21 +0600

    add animations for bes

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -3,6 +3,20 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$bes/bes1.visible = true
+	$bes/bes2.visible = false
+	$bes/bes3.visible = false
+	$bes/AP.play("idle")
+	
+	$bes2/bes2.visible = true
+	$bes2/bes1.visible = false
+	$bes2/bes3.visible = false
+	$bes2/AP.play("idle_bes2")
+	
+	$bes3/bes3.visible = true
+	$bes3/bes1.visible = false
+	$bes3/bes2.visible = false
+	$bes3/AP.play("idle_bes3")
 	pass # Replace with function body.
 
 
