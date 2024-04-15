8cd230ab60b2cb51260b3397e0b5dd0ec16536e4
Onizik <Onizik@mail.ru>
2024-04-15 23:36:49 +0600

    add new bugs

--- assets/game/animations/bes.gd
+++ assets/game/animations/bes.gd
@@ -1,21 +1,24 @@
 extends Node2D
 var go = false
-var count = 3
+var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	if go:
 		$numb.text = str(count)
 		go = false
 	pass
 
 
+
 func _on_butt_pressed():
+	count = int($numb.text)
 	$"/root/Global".spell = false
 	if $"/root/Global".sp_butt == "sp_butt1":
 		$"/root/Global".sp1 = false
@@ -33,5 +36,13 @@ func _on_butt_pressed():
 	go = true
 	if $"/root/Global".side:
 		count += int($"/root/Global".number)
-	else: count -= int($"/root/Global".number)
+	else: 
+		count -= int($"/root/Global".number)
+		print ("x " + str($"/root/Global".number))
+		if count <= 0:
+			print ("E " + str(count))
+			count = 0
+			self.visible = false
+		else: 
+			print ("a " + str(count))
 	pass # Replace with function body.
--- assets/game/animations/cat.gd
+++ assets/game/animations/cat.gd
@@ -1,14 +1,16 @@
 extends Node2D
 var go = false
-var count = 2
+var count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	if go:
 		$numb.text = str(count)
 		go = false
@@ -16,6 +18,7 @@ func _process(delta):
 
 
 func _on_butt_pressed():
+	count = int($numb.text)
 	$"/root/Global".spell = false
 	if $"/root/Global".sp_butt == "sp_butt1":
 		$"/root/Global".sp1 = false
@@ -32,5 +35,11 @@ func _on_butt_pressed():
 	go = true
 	if $"/root/Global".side:
 		count += int($"/root/Global".number)
-	else: count -= int($"/root/Global".number)
+	else: 
+		count -= int($"/root/Global".number)
+		if count <= 0:
+			print ("E " + str(count))
+			count = 0
+			self.visible = false
+	
 	pass # Replace with function body.
