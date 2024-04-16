3dcf854e58535f5a71feb6087b5ac08f95aaba4d
Onizik <Onizik@mail.ru>
2024-04-16 06:04:26 +0600

    anime kruto

--- scenes/final.gd
+++ scenes/final.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AP.play("fade")
 	if $"/root/Global".win > $"/root/Global".los1 and $"/root/Global".win > $"/root/Global".los2:
 		$text/text.text = "Friends win this battle!"
 		$win.frame = 0
@@ -18,3 +19,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_ap_animation_finished(anim_name):
+	if anim_name == "fade_in":
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	
+
+
+func _on_butt_pressed():
+	$AP.play("fade_in")
